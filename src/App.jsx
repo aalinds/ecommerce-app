68fdac54b1f45992b795bc7ea1862705200bad11
Aalind Sharma <aalind@vyaparapp.in>
2021-08-02 18:50:49 +0530

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import NavBar from './components/NavBar/NavBar';
 import HomePage from './components/HomePage/HomePage.jsx';
+import Cart from './components/Cart/Cart.jsx';
 
 const App = () => (
 	<React.Fragment>
 		<NavBar />
 		<Switch>
 			<Route exact path='/' component={HomePage} />
-			{/* <Route path='/cart' component={Cart} />
-			<Route path='/women' component={Women} />
-			<Route path='/men' component={Men} />
-			<Route path='/clothes' component={Clothes} />
-			<Route path='/accessories' component={Accessories} />
-			<Route exact path='/products/:id' component={ShowProduct} /> */}
+			<Route path='/cart' component={Cart} />
 		</Switch>
 	</React.Fragment>
 );
