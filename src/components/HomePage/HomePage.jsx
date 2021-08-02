9ab3fb0f204424b8e49a3e572a024be2c01cb6c8
Aalind Sharma <aalind@vyaparapp.in>
2021-08-02 18:49:18 +0530

    cart reducer added

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addToCart } from '../../actions/cartActions';
 
 import './HomePage.scss';
 
 class HomePage extends Component {
+	handleClick = (id) => {
+		this.props.addToCart(id);
+	};
+
 	render() {
 		return (
 			<div className='homepage'>
@@ -18,7 +23,13 @@ class HomePage extends Component {
 						<div className='collection-footer'>
 							<span className='name'>{item.name}</span>
 							<span className='price'>$ {item.price}</span>
-							<span>Add to cart</span>
+							<span
+								onClick={() => {
+									this.handleClick(item.productId);
+								}}
+							>
+								Add to cart
+							</span>
 						</div>
 					</div>
 				))}
@@ -26,11 +37,17 @@ class HomePage extends Component {
 		);
 	}
 }
-
+const mapDispatchToProps = (dispatch) => {
+	return {
+		addToCart: (id) => {
+			dispatch(addToCart(id));
+		},
+	};
+};
 const mapStateToProps = (state) => {
 	return {
 		items: state.items,
 	};
 };
 
-export default connect(mapStateToProps)(HomePage);
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
